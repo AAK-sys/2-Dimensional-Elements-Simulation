9afa54480f6e6db6a6d9ea9391d738cd6b798c09
unknown <akmk2237@gmail.com>
2021-12-17 18:13:15 -0500

    Clear Option Added

--- Simulation.java
+++ Simulation.java
@@ -88,13 +88,96 @@ public class Simulation {
 		// the correct color for that cell. The display has one
 		// cell per grid element (one-to-one) and the color of
 		// the cell is just the color of the element from the grid.
-		for (int i = 0; i < INIT_ROWS; i++) {
-			for (int j = 0; j < INIT_COLS; j++) {
+		for (int i = 0; i < grid.size(); i++) {
+			for (int j = 0; j < grid.get(0).size(); j++) {
 				display.setColor(i, j, grid.get(i).get(j).getColor());
 			}
 		}
 		// Remember: Display has a setColor(row, col, color) method,
 		// and elements have a getColor() method.
+	}
+
+	/**
+	 * Used to adjust the vertical size of the grid.
+	 * 
+	 * @param grid being worked on
+	 * @param row  desired amount of rows
+	 * @return whether the grid was changed.
+	 */
+	private boolean rowUpdate(DynamicArray<DynamicArray<Element>> grid, int row) {
+
+		int rowLength = this.grid.get(0).size();
+
+		// add block
+		if (row > this.grid.size()) {
+
+			for (int j = this.grid.size(); j < row; ++j) {
+
+				this.grid.add(0, new DynamicArray<>());
+
+				for (int i = 0; i < rowLength; ++i) {
+					this.grid.get(0).add(new Empty());
+				}
+			}
+
+			return true;
+		}
+
+		// remove block
+		else if (row < this.grid.size()) {
+
+			for (int j = this.grid.size(); j > row; --j) {
+
+				this.grid.remove(0);
+			}
+			return true;
+
+		}
+		return false;
+	}
+
+	/**
+	 * Updates the columns.
+	 * 
+	 * @param grid being worked on.
+	 * @param col  location.
+	 * @return true or false based on succesful change.
+	 */
+	private boolean colUpdate(DynamicArray<DynamicArray<Element>> grid, int col) {
+		int colLength = this.grid.size();
+
+		// add block
+		if (col > this.grid.get(0).size()) {
+
+			for (int j = this.grid.get(0).size(); j < col; ++j) {
+
+				for (int i = 0; i < colLength; ++i) {
+
+					this.grid.get(i).add(j, new Empty());
+				}
+
+			}
+
+			return true;
+
+		}
+
+		else if (col < this.grid.get(0).size()) {
+
+			for (int j = this.grid.get(0).size(); j > col; --j) {
+
+				for (int i = 0; i < colLength; ++i) {
+
+					this.grid.get(i).get(j - 1).pushLeft(grid, i, j - 1);
+					this.grid.get(i).remove(j - 1);
+				}
+			}
+
+			return true;
+		}
+
+		return false;
+
 	}
 
 	/**
@@ -111,6 +194,16 @@ public class Simulation {
 		// should double check the numbers of rows/columns is
 		// at least 1 here. Ignore the resize request if an
 		// invalid value is given (don't crash the program...).
+
+		if (numRows < 1 || numCols < 1) {
+			return false;
+		}
+
+		boolean rowDim = this.rowUpdate(this.grid, numRows);
+
+		boolean colDim = this.colUpdate(this.grid, numCols);
+
+		return rowDim || colDim;
 
 		// Add/remove all of _one_ column or row at a time until
 		// the correct size is achieved. For example, if you
@@ -141,7 +234,6 @@ public class Simulation {
 
 		// It would be very, very wise to make yourself some private
 		// helper methods to do this part of the project.
-		return false;
 	}
 
 	/**
@@ -170,7 +262,6 @@ public class Simulation {
 	public void step() {
 		int row = (int) (Math.random() * grid.size());
 		int col = (int) (Math.random() * grid.get(row).size());
-
 		grid.get(row).get(col).fall(grid, row, col);
 	}
 
